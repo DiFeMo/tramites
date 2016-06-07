dc1737991ef227983b8c1fff825b992857e53fed
Diego Monroy <difemo91@gmail.com>
2016-06-06 23:22:25 -0500

    Utilidad por mes

--- modelo/mconsulta.php
+++ modelo/mconsulta.php
@@ -1,7 +1,7 @@
 <?php
 
 	/*  
-        *   @Version: V4 25/05/16
+        *   @Version: V4 06/06/16
     */
 
 	include('controlador/conexion.php');
@@ -37,5 +37,32 @@
 		{
 			$sql = "SELECT * FROM csutilidaddia";
 			return $this->SeleccionDatos($sql);
-		}		
+		}
+		/*
+		 *función para la consulta de los datos de la salida por mes
+		 */
+		function consultar_salida_mes()
+		{
+			$sql = "SELECT month(`cssalida`.`fecha`) AS `mes`,year(`cssalida`.`fecha`) AS `year`,sum(`cssalida`.`salida`) AS `salida_mes` 
+					from `cssalida` group by month(`cssalida`.`fecha`),year(`cssalida`.`fecha`)";
+			return $this->SeleccionDatos($sql);
+		}
+		/*
+		 *función para la consulta de los datos de la entrada por mes
+		 */
+		function consultar_entrada_mes()
+		{
+			$sql = "SELECT month(`csentrada`.`fecha`) AS `mes`,year(`csentrada`.`fecha`) AS `year`,sum(`csentrada`.`entrada`) AS `entrada_mes` 
+					from `csentrada` group by month(`csentrada`.`fecha`),year(`csentrada`.`fecha`)";
+			return $this->SeleccionDatos($sql);
+		}
+		 /*
+		 *función para la consulta de los datos de la consulta UtilidadxMes
+		 */
+		function consultar_utilidad_mes()
+		{
+			$sql = "SELECT month(`csutilidaddia`.`fecha`) AS `mes`,year(`csutilidaddia`.`fecha`) AS `year`,sum(`csutilidaddia`.`utilidad`) AS `utilidad_mes` 
+					from `csutilidaddia` group by month(`csutilidaddia`.`fecha`),year(`csutilidaddia`.`fecha`)";
+			return $this->SeleccionDatos($sql);
+		}
 	}
--- vista/vcsutilidad.php
+++ vista/vcsutilidad.php
@@ -4,11 +4,18 @@
     include('modelo/mconsulta.php'); 
     $utilidad = new Mconsulta();
 
-$consultasalida = $utilidad->consultar_salida();
-$consultaentrada = $utilidad->consultar_entrada();
-$consultautilidad = $utilidad->consultar_utilidad();
+$consultasalida         = $utilidad->consultar_salida();
+$consultaentrada        = $utilidad->consultar_entrada();
+$consultautilidad       = $utilidad->consultar_utilidad();
+$consultasalidames      = $utilidad->consultar_salida_mes();
+$consultaentradames     = $utilidad->consultar_entrada_mes();
+$consultautilidadmes    = $utilidad->consultar_utilidad_mes();
 ?>
 <div class="row">
+<input type="checkbox" id="spoiler2" />
+<label for="spoiler2">Ingresos y Egresos por dia</label>
+<div class="spoiler">
+    <div class="info">
 <!--Inicio Salida-->
    <div class="col-md-4">
    <div class="etinfo">Egresos</div>
@@ -77,8 +84,96 @@ $consultautilidad = $utilidad->consultar_utilidad();
             <?php endfor; ?>
         </tbody>
     </table> 
-   </div>    
+   </div> 
 </div>
+</div>
+
+</div>
+<div class="row">
+<input type="checkbox" id="spoiler3" />
+<label for="spoiler3">Ingresos y Egresos por mes</label>
+<div class="spoiler">
+    <div class="info">
+<!--Inicio Salida mes-->
+   <div class="col-md-4">
+   <div class="etinfo">Egresos</div>
+    <table id="" class="display" cellspacing="0" width="100%">
+       <thead>
+            <tr>
+                <th colspan="12">Egresos</th>
+            </tr>
+            <tr>
+                <th>Mes</th>
+                <th>Año</th> 
+                <th>Salida</th>                  
+            </tr>
+        </thead>
+        <tbody>
+            <?php for($i=0;$i<count($consultasalidames);$i++): ?>
+                <tr>                    
+                    <td data-title='Mes'><?= $consultasalidames[$i]['mes'] ?></td>
+                    <td data-title='Año'><?= $consultasalidames[$i]['year'] ?></td>
+                    <td data-title='Salida'>$ <?= number_format($consultasalidames[$i]['salida_mes']) ?></td>  
+                </tr>
+            <?php endfor; ?>
+        </tbody>
+    </table> 
+   </div>    
+<!--Inicio Entrada mes-->
+   <div class="col-md-4">
+   <div class="etinfo">Ingresos</div>
+    <table id="" class="display" cellspacing="0" width="100%">
+       <thead>
+            <tr>
+                <th colspan="12">Ingresos</th>
+            </tr>
+            <tr>
+                <th>Mes</th>
+                <th>Año</th>
+                <th>Entrada</th>                
+            </tr>
+        </thead>
+        <tbody>
+            <?php for($i=0;$i<count($consultaentradames);$i++): ?>
+                <tr>                    
+                    <td data-title='Mes'><?= $consultaentradames[$i]['mes'] ?></td>
+                    <td data-title='Año'><?= $consultaentradames[$i]['year'] ?></td>
+                    <td data-title='Entrada'>$ <?= number_format($consultaentradames[$i]['entrada_mes']) ?></td>  
+                </tr>
+            <?php endfor; ?>
+        </tbody>
+    </table> 
+   </div>    
+<!--Inicio Utilidad mes-->
+   <div class="col-md-4">
+   <div class="etinfo">Utilidad</div>
+    <table id="" class="display" cellspacing="0" width="100%">
+       <thead>
+            <tr>
+                <th colspan="12">Utilidad</th>
+            </tr>
+            <tr>
+                <th>Mes</th>
+                <th>Año</th>
+                <th>Valor</th>                
+            </tr>
+        </thead>
+        <tbody>
+            <?php for($i=0;$i<count($consultautilidadmes);$i++): ?>
+                <tr>                    
+                    <td data-title='Mes'><?= $consultautilidadmes[$i]['mes'] ?></td>
+                    <td data-title='Año'><?= $consultautilidadmes[$i]['year'] ?></td>
+                    <td data-title='Valor'>$ <?= number_format($consultautilidadmes[$i]['utilidad_mes']) ?></td>  
+                </tr>
+            <?php endfor; ?>
+        </tbody>
+    </table> 
+   </div> 
+</div>
+</div>
+
+</div>
+<br><br>
 <script type="text/javascript">
     $(document).ready(function () {
         $(".nav li").removeClass("active");//this will remove the active class from  
